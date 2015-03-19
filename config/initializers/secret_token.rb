09b0cd8a91e6da4d6f8aeb718e7ddaa4a15b7b2f
victor-aretia <victor.aretia@gmail.com>
2015-03-18 23:24:00 -0600

    New blood

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c152e5728e5a408270ce89537cff245036118eae8b9b6e08a1cb058967d20a4ef2d52cf0bff37ba6acb81f2a116b6fd9762da31798c3231efe9d32f9aa49f4c8'
